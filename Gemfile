c0845c377c87a81ebc808d09279559ff68444dfc
Scotty Runyan <runyans7@gmail.com>
2016-03-04 21:13:31 -0500

    Omniauth from facebook working

--- Gemfile
+++ Gemfile
@@ -14,6 +14,7 @@ gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
 gem 'devise'
 gem 'pundit'
+gem 'omniauth-facebook'
 gem 'sdoc', '~> 0.4.0', group: :doc
 
 group :development, :test do
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
 require 'pry'
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
-  # :lockable, :timeoutable and :omniauthable
+  # :lockable, :timeoutable
   devise :database_authenticatable, :registerable, :recoverable,
-         :rememberable, :trackable, :validatable, :confirmable
+         :rememberable, :trackable, :validatable, :confirmable,
+         :omniauthable, :omniauth_providers => [:facebook]
 
   has_many :projects
   has_many :project_collaborations
@@ -11,5 +12,12 @@ class User < ActiveRecord::Base
 
   def project_role(project)
     project_collaborations.where("user_id = ? AND project_id = ?", self.id, project.id).first
+  end
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+    end
   end
 end
