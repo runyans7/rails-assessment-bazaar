b5fe8996d7d6fffeaab75d6b9ba51db9249d3f3e
Scotty Runyan <runyans7@gmail.com>
2016-03-04 21:25:40 -0500

    Skip email confirm signing up with omniauth

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -3,7 +3,7 @@ require 'pry'
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def facebook
     @user = User.from_omniauth(request.env["omniauth.auth"])
-    
+
     if @user.persisted?
       sign_in_and_redirect @user
       set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
